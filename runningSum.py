50cc5823c10c9559094556498f92cef9920b37f4
Hailemari <hailemariamkefale19@gmail.com>
2023-08-01 03:11:08 +0300

    Running Sum of 1d Array

---
+++ runningSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        if n == 1:
+            return nums
+        
+        for i in range(1, n):
+            nums[i] = nums[i-1] + nums[i]
+        
+        return nums

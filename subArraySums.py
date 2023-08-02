b6a698fff4399b466c9f0c31998899a22bfd3c4a
Hailemari <hailemariamkefale19@gmail.com>
2023-08-02 20:49:54 +0300

    Subarray Sums Divisible by K

---
+++ subArraySums.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        count = 0
+        remaindersCount = defaultdict(int)
+        remaindersCount[0] = 1
+
+        runningSum = 0
+        for n in nums:
+            runningSum += n
+            remainder = runningSum % k
+            count += remaindersCount[remainder]
+            remaindersCount[remainder] += 1
+
+        return count
+

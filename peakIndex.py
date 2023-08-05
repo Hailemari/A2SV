83c5ae328b84725a685d09eb8c1ca006a49ae85f
Hailemari <hailemariamkefale19@gmail.com>
2023-08-05 17:34:50 +0300

    peak index in a mountain array

---
+++ peakIndex.py
@@ -0,0 +1,13 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        start, end = 0, len(arr) - 1
+
+        while start <= end:
+            mid = start + (end - start) // 2
+
+            if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
+                return mid
+            elif arr[mid] > arr[mid - 1]:
+                start = mid
+            else:
+                end = mid

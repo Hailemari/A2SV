8707b7ea8becebf0632dc43d3e2109e41a00d404
Hailemari <hailemariamkefale19@gmail.com>
2023-08-16 21:22:10 +0300

    validate binary search tree

---
+++ validateBST.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def isValidBSTHelper(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+            
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+            
+            return (
+                isValidBSTHelper(node.left, lower, val) and
+                isValidBSTHelper(node.right, val, upper)
+            )
+        
+        return isValidBSTHelper(root)
+
+            
